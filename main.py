d341652de72a23da75042085fc818f2a3f367efe
Gregor Riegler <rieglerg85@gmail.com>
2025-01-01 13:28:59 +0100

    styling

--- main.py
+++ main.py
@@ -16,7 +16,6 @@ if(queryString.get("inputs")):
 
 def create_table(headers, rows):
     table = document.createElement("table")
-    table.style.border = "1px solid black"
     table.style.borderCollapse = "collapse"
 
     thead = document.createElement("thead")
@@ -24,8 +23,6 @@ def create_table(headers, rows):
     for header in headers:
         th = document.createElement("th")
         th.textContent = header
-        th.style.border = "1px solid black"
-        th.style.padding = "1px"
         header_row.appendChild(th)
     thead.appendChild(header_row)
     table.appendChild(thead)
@@ -39,8 +36,6 @@ def create_table(headers, rows):
                 td.innerHTML = cell.replace("?", "<span class=\"dran\">█</span>")
             else:
                 td.innerHTML = str(cell)
-            td.style.border = "1px solid black"
-            td.style.padding = "1px"
             tr.appendChild(td)
         tbody.appendChild(tr)
     table.appendChild(tbody)
@@ -60,7 +55,7 @@ def handle_keypress(_):
         
     inputs_as_string = "".join(str(i) for i in game.inputs)
     queryString.set("inputs", inputs_as_string)
-    window.history.pushState(None, "New Page", f"?{queryString}")
+    window.history.pushState(None, None, f"?{queryString}")
     draw_game(game)
 
 draw_game(game)
